a76302d31c8e429c03bad923d4e6001f38ef6f9e
issei1213 <59830008+issei1213@users.noreply.github.com>
2020-01-28 15:50:13 +0900

    メンター指摘点修正後

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -22,64 +22,62 @@ $(function(){
         //メッセージが入ったHTMLに、入れ物ごと追加
         $('.chat-main__message-list').append(insertHTML);
         $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
-        $('.chat-main__message-form__bottom-items-left')[0].reset();
-        $('.chat-main__message-form__bottom-items-left__send').prop("disabled", false);
       }
     })
     .fail(function() {
-      console.log('error');
+      alert("更新に失敗しました");
     });
   };
 
 
   var buildHTML = function(message){
     if(message.content && message.image) {
-      var html = `<div class="message" data-message-id=` + message.id + `>` +
-      `<div class="chat-main__message-list__middle-items-top">` +
-      `<div class="chat-main__message-list__middle-items-top__user-name">` +
-            message.user_name +
-            `</div>` +
-          `<div class="chat-main__message-list__middle-items-top__send-datetime">` +
-          message.created_at +
-            `</div>` +
-          `</div>` +
-        `<div class="chat-main__message-list__middle-items-message">` +
-        `<p class="lower-message__content">` +
-            message.content +
-          `</p>` +
-        `</div>` +
-        `<img src=` + message.image > + `" class = "lower-message__image" >` +
-      `</div>`
+      var html = `<div class="message" data-message-id=${message.id}>
+      <div class="chat-main__message-list__middle-items-top">
+      <div class="chat-main__message-list__middle-items-top__user-name">
+            ${message.user_name}
+            </div>
+          <div class="chat-main__message-list__middle-items-top__send-datetime">
+          ${message.created_at}
+            </div>
+          </div>
+        <div class="chat-main__message-list__middle-items-message">
+        <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+        <img src= ${message.image},class = "lower-message__image" >
+      </div>`
     } else if(message.content) {
-      var html = `<div class="message" data-message-id=` + message.id + `>` +
-      `<div class="chat-main__message-list__middle-items-top">` +
-      `<div class="chat-main__message-list__middle-items-top__user-name">` +
-            message.user_name +
-            `</div>` +
-          `<div class="chat-main__message-list__middle-items-top__send-datetime">` +
-            message.created_at +
-            `</div>` +
-          `</div>` +
-        `<div class="chat-main__message-list__middle-items-message">` +
-        `<p class="lower-message__content">` +
-            message.content +
-        `</p>` +
-        `</div>` +
-      `</div>`
+      var html = `<div class="message" data-message-id=  ${message.id}  > 
+      <div class="chat-main__message-list__middle-items-top"> 
+      <div class="chat-main__message-list__middle-items-top__user-name"> 
+            ${message.user_name} 
+            </div> 
+          <div class="chat-main__message-list__middle-items-top__send-datetime"> 
+            ${message.created_at} 
+            </div> 
+          </div> 
+        <div class="chat-main__message-list__middle-items-message"> 
+        <p class="lower-message__content"> 
+            ${message.content} 
+        </p> 
+        </div> 
+      </div>`
     } else if(message.image) {
-      var html = `<div class="message" data-message-id=` + message.id + `>` +
-      `<div class="chat-main__message-list__middle-items-top">` +
-      `<div class="chat-main__message-list__middle-items-top__user-name">` +
-            message.user_name +
-            `</div>` +
-          `<div class="chat-main__message-list__middle-items-top__send-datetime">` +
-          message.created_at +
-            `</div>` +
-          `</div>` +
-        `<div class="chat-main__message-list__middle-items-message">` +
-        `<img src=` + message.image > + `" class = "lower-message__image" >` +
-        `</div>` +
-      `</div>`
+      var html = `<div class="message" data-message-id=  ${message.id}  > 
+      <div class="chat-main__message-list__middle-items-top"> 
+      <div class="chat-main__message-list__middle-items-top__user-name"> 
+            ${message.user_name} 
+            </div> 
+          <div class="chat-main__message-list__middle-items-top__send-datetime"> 
+          ${message.created_at} 
+            </div> 
+          </div> 
+        <div class="chat-main__message-list__middle-items-message"> 
+        <img src= ${message.image} class = "lower-message__image" > 
+        </div> 
+      </div>`
     };
     return html;
     };
@@ -99,11 +97,6 @@ $(function(){
       var html = buildHTML(message)
       if(message.length !== 0){
         $('.chat-main__message-list').append(html);
-        // var insertHTML = '';
-        // $.each(messages, function(i, message) {
-        //   insertHTML += buildHTML(message)
-        // });
-        // $('.chat-main__message-list').append(insertHTML);
         $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
         $('.chat-main__message-form__bottom-items-left')[0].reset();
         $('.chat-main__message-form__bottom-items-left__send').prop("disabled", false);
