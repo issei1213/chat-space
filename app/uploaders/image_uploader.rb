3c772277fd45636bf51898ac07c4600d4adc7127
issei1213 <59830008+issei1213@users.noreply.github.com>
2020-01-29 16:22:58 +0900

    Update image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,6 +7,6 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fit: [800,800]
+  process resize_to_fit: [200,200]
 
 end
